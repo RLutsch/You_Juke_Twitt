bf0c77df3061ca4eb326439dcaabf753c523518e
rlutsch <ross.lutsch@gmail.com>
2016-09-07 15:59:54 +0200

    styling

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ var T = new Twit({
 });
 
 var stream = T.stream("statuses/filter", {
-    track: "#WTC, @wethinkcode, #highschoolin5words"
+    track: "#wtc_request"
 });
 
 stream.on("tweet", function(status) {
